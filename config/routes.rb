b1c9698196d0617858f4d71d466aad3729a4a80f
Sonja Wilmsen <s_wilmsen@hotmail.com>
2016-06-16 13:28:24 +0200

    Toggle like posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  resources :posts
+  resources :posts do
+    resources :likes
+  end
 
    root to: "posts#index"
    get 'user_posts/:user_id' => 'posts#user', as: :user_posts
